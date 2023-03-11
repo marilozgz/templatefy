c4a2159eb04d54cd9cd42d6cd14be19e638d6685
marilozgz <crazygipsypuaj@gmail.com>
2023-03-11 23:12:04 +0100

    update next config vercel

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
-  env: {
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY
-  },
 };
 
 module.exports = nextConfig;
