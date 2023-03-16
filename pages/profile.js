c1c8386543a1438d122a854d1bc2a01c2d317f71
marilozgz <crazygipsypuaj@gmail.com>
2023-03-16 23:10:35 +0100

    fix: remove stripe js need create profile app

--- pages/profile.js
+++ pages/profile.js
@@ -1,25 +1,9 @@
-import Head from 'next/head'
-import { Nunito } from '@next/font/google'
-import { Navbar } from '@/components/Navbar'
-import { Footer } from '@/components/Footer'
-import { Drawer } from '@/components/Drawer'
-const nunito = Nunito({ subsets: ['latin'] })
-import Script from 'next/script'
-export default function Profile() {
-  
-  return (
-    <>
-    <Head>
-        <title>Account area</title>
-        <Script src="https://js.stripe.com/v3/"/>
-      </Head>
-      <main>
-        
-        <Navbar />
-        <Drawer />
-        <Footer />
-      </main>
-     
-    </>
+ const Profile  = () => {
+  return(
+    <div>
+      <h1>Profile</h1>
+    </div>
   )
 }
+
+export default Profile
